d80ed1c4dc9c712cd350527559f7a0383d5af857
olalekankin <olalekankin@gmail.com>
2023-06-09 14:31:20 +0100

    'corrections'

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Mail; 
 
-class ContactController extends Controller
+class contactController extends Controller
 {
-   public function contact()
-   {
-     return view('support')
-   }
+    public function contact()
+    {
+        return view('support');
+    }
+
+    public function sendEmail(Request $request)
+    {
+        $details = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'message' => $request->message 
+        ];
+
+        Mail::to('hello@visionarymomasfreemium.com')->send(new ContactMail($details));
+        return back()->with('message_sent', 'Your message has been sent successfully');
+    }
 }
